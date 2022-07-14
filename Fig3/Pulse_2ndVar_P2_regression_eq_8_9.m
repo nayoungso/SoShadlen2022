b68645240a8c9f7f48d0c173146fe6fe7a2f19bc
nayoungso <nayoung.so@gmail.com>
2022-07-14 14:29:03 -0400

    Update Pulse_2ndVar_P2_regression_eq_8_9.m
    updating the file paths

--- Fig3/Pulse_2ndVar_P2_regression_eq_8_9.m
+++ Fig3/Pulse_2ndVar_P2_regression_eq_8_9.m
@@ -1,4 +1,4 @@
-%function Pulse_2ndVar_P2_regression_eq_3_4
+%function Pulse_2ndVar_P2_regression_eq_8_9
 
 
 % correct trials only and remove choice term
@@ -7,16 +7,16 @@
 % C1 = coherence of P1, C2 = coherence of P2
 
 % two separate regressions: 
-% R1 = a0+a1C1          (eq.3)
-% R2 = (a0+a1C1)+b1C2   (eq.4)
+% R1 = a0+a1C1          (eq.8)
+% R2 = (a0+a1C1)+b1C2   (eq.9)
 
 
-path = ['~/Dropbox/plxdata/Pulse/Vprobe/prelim_groups/coh_dep_dynamics_based_consec3_p_05_base_adjusted/allo/follower/']
+path = ['~/Two-pulse task, 2nd variant/Supporter']
 
-filename_a = dir(strcat(path,'/34/r/','*_DSP*.mat'));
-filename_b = dir(strcat(path,'/34/g/','*_DSP*.mat'));
-filename_c = dir(strcat(path,'/35/r/','*_DSP*.mat'));
-filename_d = dir(strcat(path,'/35/g/','*_DSP*.mat'));
+filename_a = dir(strcat(path,'/A/r/','*_DSP*.mat'));    % configuration A, up/right-preferring neurons
+filename_b = dir(strcat(path,'/A/g/','*_DSP*.mat'));    % configuration A, down/left-preferring neurons
+filename_c = dir(strcat(path,'/B/r/','*_DSP*.mat'));    % configuration B, up/right-preferring neurons
+filename_d = dir(strcat(path,'/B/g/','*_DSP*.mat'));    % configuration B, down/left-preferring neurons
 
 
 binwidth = 100;
@@ -38,13 +38,13 @@ for p=1:4
     clear filename
     
     if p==1
-        taskid = 34; filename = filename_a; filepath = [path,'/34/r/'];
+        taskid = 34; filename = filename_a; filepath = [path,'/A/r/'];
     elseif p==2
-        taskid = 34; filename = filename_b; filepath = [path,'/34/g/'];
+        taskid = 34; filename = filename_b; filepath = [path,'/A/g/'];
     elseif p==3
-        taskid = 35; filename = filename_c; filepath = [path,'/35/r/'];
+        taskid = 35; filename = filename_c; filepath = [path,'/B/r/'];
     elseif p==4
-        taskid = 35; filename = filename_d; filepath = [path,'/35/g/'];
+        taskid = 35; filename = filename_d; filepath = [path,'/B/g/'];
     end
     
     for i = 1:length(filename)
