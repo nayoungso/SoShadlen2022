a6d69ceff7fb80615c2eace724764d0e5bc992e3
nayoungso <nayoung.so@gmail.com>
2022-07-14 14:49:30 -0400

    Update VD_behav_pmf.m
    updating the file paths

--- Fig1/VD_behav_pmf.m
+++ Fig1/VD_behav_pmf.m
@@ -6,15 +6,16 @@ figure(10); clf;
 for monkey_id = 1:2
     
     clear filename
+    path = '~/Variable duration task/BehaviorOnly/';
+    
     if monkey_id==1
-        path = ['~/Dropbox/plxdata/RDM_IEM/BrBehav/ME/'];
+        filename = dir(strcat(path,'Br*behav_me.mat'));
         titlestring = 'Monkey B'
     else
-        path = ['~/Dropbox/plxdata/RDM_IEM/HBehav/ME/'];
+        filename = dir(strcat(path,'H*behav_me.mat'));
         titlestring = 'Monkey H'
     end
     
-filename = dir(strcat(path,'*behav_me.mat'))
 
 
 trialno = 0;
